9c85e1df00392bbfef9cdf60c5aac3e907aa26a0
Josh Wright <wright.jjw@gmail.com>
2020-12-10 12:49:12 -0600

    added most of firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAJYZMdsFMSDxy9nq0lwJMJnhMXUkuhto0",
+    authDomain: "cs336-chat-final.firebaseapp.com",
+    projectId: "cs336-chat-final",
+    storageBucket: "cs336-chat-final.appspot.com",
+    messagingSenderId: "948282100",
+    appId: "1:948282100:web:c94ff885939881fc38b4a1"
+  },
 };
 
 /*
