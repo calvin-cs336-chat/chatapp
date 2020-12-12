7efd8aaf0b33323b481375df2ab7249c8ca30046
Josh Wright <wright.jjw@gmail.com>
2020-12-12 16:54:08 -0600

    log in functions

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    public auth: AngularFireAuth,
+    public toastCtl: ToastController,
+    ) {}
+
+signup(email: string, password: string): void {
+  this.auth.createUserWithEmailAndPassword(email, password).then(() => {
+    // the user is signed in
+  })
+  .catch((err) => {
+    this.createErrorToast(err.message);
+  });
+}
+
+login(email: string, password: string): void {
+  this.auth.signInWithEmailAndPassword(email, password).then(() => {
+    // the user is signed in
+  })
+  .catch((err) => {
+    this.createErrorToast(err.message);
+  });
+}
+
+async createErrorToast(msg: string) {
+  const toast = await this.toastCtl.create({
+      message: msg,
+      duration: 3000,
+    });
+  }
 
 }
