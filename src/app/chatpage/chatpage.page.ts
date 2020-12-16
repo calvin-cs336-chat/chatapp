e53521806040f5efae95118f22ddc035b795eb01
Josh Wright <wright.jjw@gmail.com>
2020-12-15 20:56:20 -0600

    removed unused code

--- src/app/chatpage/chatpage.page.ts
+++ src/app/chatpage/chatpage.page.ts
@@ -6,7 +6,6 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { DBChatMsg, DBUser } from '../interfaces'
 import * as firebase from 'firebase';
 
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx'
 import { File } from '@ionic-native/file/ngx'
 import { createErrorToast } from '../app.component';
 
@@ -17,61 +16,15 @@ import { createErrorToast } from '../app.component';
 })
 export class ChatpagePage implements OnInit {
   messages: DBChatMsg[] = [];
-  imgURL;
-  pictures:any=[];
-  likeCount: number = 0;
-  dislikeCount:number = 0;
-
-
-  textChat = [
-    
-    {
-      user: "Mark",
-      dateSent: 1554093856000,
-      msg:"Yo!"
-    },
-    {
-      user: "Ian",
-      dateSent: 1554097156000,
-      msg:"Hello"
-    },
-    {
-      user: "Josh",
-      dateSent: 1554093856000,
-      msg:"Hey!"
-    },
-    {
-      user: "Mark",
-      dateSent: 1554092056000,
-      msg:"I am bored!"
-    }
-    
-  ]
-
-  currentUser = 'Mark'
+  user: DBUser;
   enterMsg = '';
   @ViewChild(IonContent) content: IonContent
-
-  sendText(){
-    this.textChat.push({
-      user: 'Mark',
-      dateSent: new Date().getTime(),
-      msg: this.enterMsg
-    });
-
-    this.enterMsg = '';
-
-    setTimeout(() => {
-      this.content.scrollToBottom(200);
-    })
-  }
 
   constructor(
       private db: AngularFirestore,
       public auth: AngularFireAuth,
       private router: Router,
       public toastCtl: ToastController,
-      private camera: Camera,
       public file: File,
     ) {
     this.db.collection<DBChatMsg>('/chat').valueChanges().subscribe((res) => {
@@ -103,7 +56,6 @@ export class ChatpagePage implements OnInit {
 
   }
 
-  user: DBUser;
 
   sendMsg(): void {
     const time = firebase.default.firestore.Timestamp.now();
@@ -114,22 +66,9 @@ export class ChatpagePage implements OnInit {
       likes: [],
     });
     this.enterMsg = '';
-  }
-
-  chatCamera(){
-    const options: CameraOptions={
-      quality:100,
-      mediaType:this.camera.MediaType.PICTURE,
-      destinationType:this.camera.DestinationType.FILE_URI,
-      encodingType:this.camera.EncodingType.JPEG
-    }
-    this.camera.getPicture().then((ImageData)=>{
-      let filename = ImageData.substring(ImageData.lastIndexOf('/')+1);
-      let path = ImageData.substring(0, ImageData.lastIndexOf('/')+1);
-      this.file.readAsDataURL(path, filename).then((base64data)=>{
-        this.pictures.push(base64data);
-      })
-    })
+    setTimeout(() => {
+      this.content.scrollToBottom(200);
+    });
   }
 
   like(msg: DBChatMsg) {
@@ -144,15 +83,6 @@ export class ChatpagePage implements OnInit {
     });
   }
   
-  likeButton(){
-    this.likeCount++;
-  }
-
-  dislikeButton(){
-    this.dislikeCount++;
-  }
-
-
   ngOnInit() {
   }
 
